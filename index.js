0c887cc918543e48beeaa7f9841809ae940b293d
ndungu1mac <macgeorgendungu@gmail.com>
2022-08-20 14:41:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+      window.customerName = window.customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+      window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+      window.bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'not bob';
+function changeLeastFavoriteCustomer(newLeastFavoriteCustomer) {
+      leastFavoriteCustomer = newLeastFavoriteCustomer;
+}n
